c085ac2e3c6ba4e9265ecccd4fef060373b63f4e
Lilith Wittmann <mail@lilithwittmann.de>
2022-01-01 23:45:17 +0100

    package naming…

--- lambda.py
+++ lambda.py
@@ -1,4 +1,3 @@
-from mangum import Mangum
 import sentry_sdk
 sentry_sdk.init(
     "https://34f8c1e12f5b4db685e53599776be4c9@o1094272.ingest.sentry.io/6130501",
@@ -8,7 +7,10 @@ sentry_sdk.init(
     # We recommend adjusting this value in production.
     traces_sample_rate=1.0
 )
+
 from main import app
+from mangum import Mangum
+
 
 @app.get("/",  tags=["Endpoint Test"])
 def main_endpoint_test():

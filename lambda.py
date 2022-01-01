9c6844123bd1007c56ef639744415b42e12c595c
Lilith Wittmann <mail@lilithwittmann.de>
2022-01-02 00:40:20 +0100

    another try with docker

--- lambda.py
+++ lambda.py
@@ -1,7 +1,3 @@
-try:
-    import unzip_requirements
-except ImportError:
-    pass
 import sentry_sdk
 sentry_sdk.init(
     "https://34f8c1e12f5b4db685e53599776be4c9@o1094272.ingest.sentry.io/6130501",

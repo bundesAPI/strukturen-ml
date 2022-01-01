423377ddec03852917dec2cb4ce1e2be2d2576f7
Lilith Wittmann <mail@lilithwittmann.de>
2022-01-01 20:52:18 +0100

    add magnum

---
+++ lambda.py
@@ -0,0 +1,8 @@
+from mangum import Mangum
+from main import app
+
+@app.get("/",  tags=["Endpoint Test"])
+def main_endpoint_test():
+    return {"message": "Just another strukturen API! [orgchart-ml]"}
+
+handler = Mangum(app)

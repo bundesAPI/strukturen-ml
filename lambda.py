70c0b4a7ca75be8c5be96a7acd96e3f9c469e8bd
Lilith Wittmann <mail@lilithwittmann.de>
2022-01-01 23:05:35 +0100

    add sentry

--- lambda.py
+++ lambda.py
@@ -3,6 +3,15 @@ try:
 except ImportError:
     pass
 from mangum import Mangum
+import sentry_sdk
+sentry_sdk.init(
+    "https://34f8c1e12f5b4db685e53599776be4c9@o1094272.ingest.sentry.io/6130501",
+
+    # Set traces_sample_rate to 1.0 to capture 100%
+    # of transactions for performance monitoring.
+    # We recommend adjusting this value in production.
+    traces_sample_rate=1.0
+)
 from main import app
 
 @app.get("/",  tags=["Endpoint Test"])

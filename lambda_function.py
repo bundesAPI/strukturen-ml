8dbb438218288a311b730a0154dd2c8e5aa4b1a1
Lilith Wittmann <mail@lilithwittmann.de>
2022-01-02 05:18:57 +0100

    credentials

--- lambda_function.py
+++ lambda_function.py
@@ -1,6 +1,8 @@
+import os
+
 import sentry_sdk
 sentry_sdk.init(
-    "https://34f8c1e12f5b4db685e53599776be4c9@o1094272.ingest.sentry.io/6130501",
+    os.getenv("SENTRY_DSN", None),
 
     # Set traces_sample_rate to 1.0 to capture 100%
     # of transactions for performance monitoring.

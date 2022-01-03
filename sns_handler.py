ae2b174c785a8688d86ac0d255249a57d052cd06
Lilith Wittmann <mail@lilithwittmann.de>
2022-01-03 22:09:05 +0100

    add custom sns handlers

---
+++ sns_handler.py
@@ -0,0 +1,11 @@
+from __future__ import print_function
+import json
+
+print("Loading function")
+
+
+def sns_handler(event, context):
+    # print("Received event: " + json.dumps(event, indent=2))
+    message = event["Records"][0]["Sns"]["Message"]
+    print("From SNS: " + message)
+    return message

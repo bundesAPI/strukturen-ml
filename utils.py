16d04e8768765e0fff90acd65a52063fd44765e2
Martin <mhilde@gmx.de>
2022-01-30 17:49:16 +0100

    Fix some extra ligature characters

--- utils.py
+++ utils.py
@@ -32,7 +32,10 @@ def get_client(domain, client_id, client_secret):
 
 def replace_weird_characters(text):
     """sometimes some wierd unicode characters appear - lets just replace them for now and care about it later™"""
-    text = text.replace("�", "ti")
+    replacementDict = {"�":"ti", "ﬀ":"ff", "ﬁ": "fi", "ﬂ": "fl", "ﬃ": "ffi", "ﬄ": "ffl"}
+    for key in replacementDict.keys():
+        if key in text:
+            text = text.replace(key, replacementDict[key])
     return text
 
 
